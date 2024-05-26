f5ca0b7c22e0bb63282b6d1a4e2c79fe6e2aa0b8
Satyam <satyam@bluesapling06s-MacBook-Air.local>
2024-05-26 10:43:31 +0530

    update index.js file.

--- index.js
+++ index.js
@@ -62,14 +62,14 @@ app.use(passport.session());
 app.use(passport.setAuthenticatedUser)
 
 app.use(flash());
-app.use((req, res, next) => {
-  res.locals.flash = req.flash();
-  next();
-});
-app.get('/', (req, res) => {
-  // Assuming you render the EJS template here
-  res.render('index', { flash: req.flash() });
-});
+// app.use((req, res, next) => {
+//   res.locals.flash = req.flash();
+//   next();
+// });
+// app.get('/', (req, res) => {
+//   // Assuming you render the EJS template here
+//   res.render('index', { flash: req.flash() });
+// });
 
 app.use(customMiddleware);
 
